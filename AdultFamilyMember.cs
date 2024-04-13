6dc8d08ddbf58776820da407f151bf1132223c47
IlyTom <126102373+IlyTom@users.noreply.github.com>
2024-04-13 17:58:02 +0300

    Fix

--- AdultFamilyMember.cs
+++ AdultFamilyMember.cs
@@ -6,20 +6,23 @@ using System.Threading.Tasks;
 
 namespace FamilyTree
 {
-    internal class AdultFamilyMember:FamilyMember
+    internal class AdultFamilyMember : FamilyMember
     {
         public FamilyMember[] Children { get; set; }
         public AdultFamilyMember Spouse { get; set; }
-        
+
         public override void Print(int indent = 0)
         {
-            Console.WriteLine($"{new String(' ', indent)}|-- {this.Name}");
             if (Spouse != null)
             {
-                Console.WriteLine($"{new String(' ',indent)}  |-- Супруг(а) {Spouse.Name}");
+                Console.WriteLine($"{new String(' ', indent)}|-- {this.Name} |-- Супруг(а) {Spouse.Name}");
+            }
+            else
+            {
+                base.Print(indent);
             }
             foreach (var child in Children)
-                child.Print(indent+2);
+                child.Print(indent + 2);
         }
     }
 }
